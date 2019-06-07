aabd6562d5a0579d21f94374bfd67798cc11baf8
choiseongjun <csj2702@naver.com>
2019-06-07 18:35:25 +0900

    .

--- src/main/java/go/shop/com/board/domain/DealBoardFile.java
+++ src/main/java/go/shop/com/board/domain/DealBoardFile.java
@@ -18,19 +18,19 @@ import lombok.Data;
 */
 @Entity
 @Table(name="tb_dealboardfile")
-@Data
+
 public class DealBoardFile {
 
 	 @Id
 	 @Column(name = "dealboardfile_seq")
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
-	 private Integer fileno; //파일 시퀀..
+	 private Long fileno; //파일 시퀀..
 
 	 @ManyToOne
 	 @JoinColumn(name = "dealboard_seq")
 	 private DealBoard dealboard;
-	
-	 @Column(name = "dealboardfile_name")
+
+	@Column(name = "dealboardfile_name")
 	 private String filename;//파일이름..
 	 @Column(name = "dealboardfile_realname")
 	 private String realname;//실제이름..
@@ -40,4 +40,63 @@ public class DealBoardFile {
 	 private String path;//경로..
 	 @Column(name = "dealboardfile_type")
 	 private String fileType;//파일타입..
+	public DealBoard getDealboard() {
+		return dealboard;
+	}
+	public void setDealboard(DealBoard dealboard) {
+		this.dealboard = dealboard;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public long getFilesize() {
+		return filesize;
+	}
+	public void setFilesize(long filesize) {
+		this.filesize = filesize;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getFileType() {
+		return fileType;
+	}
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+	
+	 public Long getFileno() {
+		return fileno;
+	}
+	public void setFileno(Long fileno) {
+		this.fileno = fileno;
+	}
+	public DealBoardFile(Long fileno, DealBoard dealboard, String filename, String realname, long filesize, String path,
+			String fileType) {
+		super();
+		this.fileno = fileno;
+		this.dealboard = dealboard;
+		this.filename = filename;
+		this.realname = realname;
+		this.filesize = filesize;
+		this.path = path;
+		this.fileType = fileType;
+	}
+	public DealBoardFile() {
+		
+	}
+	
+	 
 }
