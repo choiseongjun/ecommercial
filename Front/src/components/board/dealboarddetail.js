34b052f43a1e27f6d6245c02887693cb4b9a7638
csj2702 <csj2702@naver.com>
2019-06-14 10:11:35 +0900

    게시판 디테일 이벤트처리

--- Front/src/components/board/dealboarddetail.js
+++ Front/src/components/board/dealboarddetail.js
@@ -6,25 +6,40 @@ import { connect } from 'react-redux';
 class dealboarddetail extends Component{
     constructor(props){
       super(props);
-      
-      
+
+      this.inputFormHandler = this.inputFormHandler.bind(this);
+      this.onbrdmemoChange = this.onbrdmemoChange.bind(this);
+    
     this.state = {
             dealbrdno : this.props.match.params.id
            ,dealboardone:[]
-        
-        };
-       
-    }
+           ,brdmemo:this.props.dealboard.dealboardone[0] == null ? null : this.props.dealboard.dealboardone[0].brdmemo
+           ,brdtitle:''
 
-    componentDidMount (){
+        };
+      
+    }
+   
+    inputFormHandler(e) {
+        this.setState({ [e.target.name]: e.target.value })
+    };
+    onbrdmemoChange(e) {
+        console.log(e.target.value)
+      
+        this.setState({ brdmemo: e.target.value });
+    }
+    componentWillMount(){
         this.props.selectone_dealBoard(this.state.dealbrdno);
         
+    }
+    componentDidMount (){
+       
        
     }
-      
+   
     render(){
        
-        
+        console.log(this.props.dealboard.dealboardone[0] == null ? null : this.props.dealboard.dealboardone[0].brdmemo)
         return(
         <form className="form-horizontal" onSubmit={this.onSubmit}>
             <div className="div-form">
@@ -34,16 +49,16 @@ class dealboarddetail extends Component{
                  
                     <input type="title" className="form-control" id="brdtitle" placeholder="Enter email" name="email"
                       value={this.props.dealboard.dealboardone[0] == null ? null : this.props.dealboard.dealboardone[0].brdtitle}
-                      onChange={this.onbrdtitleChange}>{this.props.dealboard.dealboardone.brdmemo}</input>
+                      onChange={this.inputFormHandler}></input>
                 </div>
                 <div className="form-group">
                     <label >내용</label>
-                    <textarea className="form-control" rows="5" id="content"
+                    <textarea className="form-control" rows="5" id="content"  name="brdmemo"
                     value={this.props.dealboard.dealboardone[0] == null ? null : this.props.dealboard.dealboardone[0].brdmemo}
                     onChange={this.onbrdmemoChange}>  </textarea>
                 </div>
                 <div className="div-button">
-                    <button type="submit" className="btn btn-primary" onClick={this._boardWrite}>Submit</button>
+                    <button type="submit" className="btn btn-primary" onClick={this._boardWrite}>글수정</button>
                 </div>
             </div>
         </form>
